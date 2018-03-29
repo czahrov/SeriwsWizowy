bff03ef2569dcda19c92b2af45b5698289a4a0fc
Dawid Worhacz <worhacz.dawid@gmail.com>
2018-03-29 17:57:17 +0200

    Podstrona kontakt

--- serwiswizowy/template/page/kontakt.php
+++ serwiswizowy/template/page/kontakt.php
@@ -1,3 +1,83 @@
+<?php
+	require get_template_directory() . "/php/PHPMailer/PHPMailer.php";
+	require get_template_directory() . "/php/PHPMailer/Exception.php";
+	
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	
+	if( !empty( $_POST ) ){
+		
+		$mail = new PHPMailer(true);
+		try{
+			$mail->CharSet = 'utf8';
+			$mail->Encoding = 'base64';
+			$mail->setLanguage( 'pl', get_template_directory() . "/php/PHPMailer/" );
+			
+			$mail->setFrom( "noreply@{$_SERVER['HTTP_HOST']}", 'serwiswizowy.pl' );
+			if( DMODE ){
+				$mail->addAddress( 'sprytne@scepter.pl' );
+			}
+			else{
+				$mail->addAddress( $formularz['Kontakt_-_email'] );
+				// $mail->addAddress( 'biuro@serwiswizowy.com' );
+			}
+			$mail->AddReplyTo( $_POST['mail'], "{$_POST['imie']} {$_POST['nazwisko']}" );
+			$mail->Subject = sprintf(
+				'%s przesyła pytanie',
+				$_POST['imie']
+				
+			);
+			$mail->Body = sprintf(
+				'Formularz kontaktowy
+--------------------------
+%s %s
+email: %s
+telefon kontaktowy: %s
+
+Wiadomość:
+%s
+
+---
+Wiadomość wygenerowana automatycznie na stronie %s',
+				$_POST['imie'],
+				$_POST['nazwisko'],
+				$_POST['mail'],
+				$_POST['tel'],
+				$_POST['message'],
+				home_url( 'kontakt' )
+				
+			);
+			
+			if( DMODE ) echo "<!--<div>{$mail->Body}</div>-->";
+			
+			$mail->send();
+			
+			$mail_status = "<div id='msg' class='d-flex justify-content-center ok'>Mail został wysłany pomyślnie</div>";
+			
+		}
+		catch( Exception $e ){
+			$mail_status = sprintf(
+				"<div id='msg' class='d-flex justify-content-center err'>Próba wysłania maila zakończyła się niepowodzeniem.<br>Powód: %s</div>",
+				$mail->ErrorInfo
+				
+			);
+			
+		}
+		
+	}
+	
+	if( DMODE ){
+		echo "<!--<div>";
+		print_r( $_POST );
+		echo "</div>-->";
+		
+	}
+	
+	$kontakt = get_post_meta( get_post()->ID, 'kontakt', true );
+	$adresowe = get_post_meta( get_post()->ID, 'dane_adresowe', true );
+	$konto = get_post_meta( get_post()->ID, 'konto_bankowe', true );
+	
+?>
 <?php printBreadcrumb(); ?>
 <div class="white-page kontakt">
 	<div class="container">
@@ -6,6 +86,9 @@
 		</div>
 	</div>
 	<!-- container -->
+	<?php
+		if( isset( $mail_status ) ) echo $mail_status;
+	?>
 	<div class="violet-bg">
 		<div class="container">
 			<div class="box d-flex justify-content-between flex-wrap fast-contact">
@@ -19,15 +102,7 @@
 					</div>
 					<div class="contact-content">
 						<h4>Kontakt</h4>
-						<p>
-							<a href="tel:+48511717110">tel: 511 717 110</a>
-						</p>
-						<p>
-							<a href='mailto:serwiswizowy@serwiswizowy.com'>serwiswizowy@serwiswizowy.com</a>
-						</p>
-						<p>
-							<a href='mailto:pomocwiza@serwiswizowy.com'>pomocwiza@serwiswizowy.com</a>
-						</p>
+						<?php echo apply_filters( 'the_content', $kontakt ); ?>
 					</div>
 				</div>
 				<!-- / col-12-->
@@ -37,7 +112,7 @@
 					</div>
 					<div class="contact-content">
 						<h4>Dane adresowe</h4>
-						<p>spółka Filmar Sp. z o.o.,<br> z siedzibą w Warszawie,<br> ul. Człuchowska 74/15,<br> 01-360 Warszawa</p>
+						<?php echo apply_filters( 'the_content', $adresowe ); ?>
 					</div>
 				</div>
 				<!-- / col-12-->
@@ -47,7 +122,7 @@
 					</div>
 					<div class="contact-content">
 						<h4>Konto bankowe</h4>
-						<p>30 1950 0001 2006 0053 9255 0002</p>
+						<?php echo apply_filters( 'the_content', $konto ); ?>
 					</div>
 				</div>
 				<!-- / col-12-->
@@ -64,15 +139,6 @@
 				<p>Masz pytanie? Wyślij do nas wiadomość</p>
 			</div>
 			<!-- / page-title col-12 col-md-3 -->
-			<!--
-			<?php
-				if( !empty( $_POST ) ){
-					print_r( $_POST );
-					
-				}
-				
-			?>
-			-->
 			<div class="formularz page-title col-12 col-md-9 contact-form-section-title  d-flex align-items-center no-gutters">
 				<form id='kontakt_form' class="d-flex flex-wrap" method='post'>
 					<div class="form-single flex-column d-flex col-12 col-md-6">
@@ -95,15 +161,15 @@
 						<label for="name">Treść Twojej wiadomości</label>
 						<textarea name="message" id="message" required pattern='^[^<>]+$' title='Niedozwolone znaki: > oraz <'></textarea>
 					</div>
-					<div class="msg-button ml-auto">
+					<button class="msg-button ml-auto" type='submit'>
 						<a class="hit-box"></a>
 						<p class="msg-btn"> wyślij wiadomość</p>
 						<i class="fa fa-angle-right fa-1x arrow-send"></i>
-					</div>
-					<input type='submit' hidden>
+					</button>
 					
 					<!-- / d-flex flex-wrap col -->
 				</form>
+				
 			</div>
 			<!-- / page-title col-12 col-md-3 -->
 		</div>
